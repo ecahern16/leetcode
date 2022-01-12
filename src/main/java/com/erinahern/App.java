71715a42edcb905201f5575d3180554a982531f8
Erin Ahern <erin@erinahern.com>
2022-01-12 14:53:57 -0500

    My second leetcode, but I'm still working on it.

--- src/main/java/com/erinahern/App.java
+++ src/main/java/com/erinahern/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+
+       /* BinarySearch704 binarySearch704 = new BinarySearch704();
+        int[] nums = {2, 3, 6, 8, 10, 15, 18, 21};
+        int answer = binarySearch704.search(nums, 18);
+        System.out.println(answer); */
+
+        //2126753390
+       // 1702766719
+        FirstBadVersion278 firstBadVersion278 = new FirstBadVersion278();
+        int answer = firstBadVersion278.firstBadVersion(2126753390);
+
+
+
     }
 }
--- src/main/java/com/erinahern/FirstBadVersion278.java
+++ src/main/java/com/erinahern/FirstBadVersion278.java
@@ -4,27 +4,22 @@ public class FirstBadVersion278 {
     public int firstBadVersion(int n) {
         int left = 0;
         int right = n - 1;
-        int mid = (left + right)/2;
+        int mid;
+        int result = n;
         while (left <= right){
-            if (isBadVersion(mid) == false){
-                if(isBadVersion(mid+1) == true){
-                    return mid+1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (isBadVersion(mid-1) ==false){
-                    return mid;
-                }
-                right = mid - 1;
-            }
             mid = (left + right)/2;
+            if (isBadVersion(mid) == true){
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        return 0;
+        return result;
     }
 
     boolean isBadVersion(int version) {
-        if (version > 12){
+        if (version > 1702766719){
             return true;
         } else {
             return false;
