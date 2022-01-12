cb0b6ed7e1e91da404bc7032428590efd0b0a19a
Erin Ahern <erin@erinahern.com>
2022-01-12 13:50:54 -0500

    Initial commit

---
+++ src/main/java/com/erinahern/App.java
@@ -0,0 +1,13 @@
+package com.erinahern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
